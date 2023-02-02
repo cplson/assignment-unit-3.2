d36c11126cbf0163de563c761b8837a3d0152b41
James Jacobsen <edjcjacobsen@gmail.com>
2023-02-02 14:28:42 -0600

    Completed 1.d

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -37,7 +37,9 @@ for(let i = 2; i <= 10; i += 2){
 }
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('1.d: STRETCH: countdown from 5 to 0');
-
+for(let i = 5; i >= 0; i--){
+  console.log(i);
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
