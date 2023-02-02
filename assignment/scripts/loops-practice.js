1409b30575b1398a30d069a4389868585418a7d0
James Jacobsen <edjcjacobsen@gmail.com>
2023-02-02 14:12:29 -0600

    Completed 1.a

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,7 +13,11 @@ for (let i=0; i<4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
-
+//I needed to change the condition: i < 5.
+console.log("1.a");
+for(let i = 0; i < 5; i++){
+  console.log(i);
+}
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
