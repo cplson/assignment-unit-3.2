87899d8e8c0df14283b0eb6d57aeabf74da5f8a4
James Jacobsen <edjcjacobsen@gmail.com>
2023-02-02 14:38:27 -0600

    Completed 3.a

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -55,10 +55,14 @@ for(star of stars){
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
-console.log('Some stars using while:');
-
+console.log('3.a: Some stars using while:');
+let i = 0;
+while(i < stars.length){
+  console.log(stars[i]);
+  i++;
+}
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
-console.log('count from 0 to 5');
+console.log('3.b: count from 0 to 5');
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
-console.log('count backwards from 10 to 5');
+console.log('3.c: count backwards from 10 to 5');
